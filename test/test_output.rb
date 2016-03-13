c71c2316ba04b5d2a37699a01e90f36cfa2a30fd
raohmaru <namoracon@gmail.com>
2016-03-13 21:13:01 +0100

    Added more unit tests

--- test/test_output.rb
+++ test/test_output.rb
@@ -1,30 +1,66 @@
 require "test/unit"
+require_relative "../lib/lackey2mse"
 require 'rubygems'
 require 'zip'
- 
+
 class TestOutput < Test::Unit::TestCase
   
   def setup
-    @output_folder = 'test/files/'
-    @expected_file = 'prgd.mse-set'
-    @expected_file_entry = 'set'
+    @output_folder = 'test/files'
+    @deck_file = 'test/decks/programmingdeck.txt'
+    @extension = 'mse-set'
   end
  
   def teardown
     
   end
  
-  def test_file
-    assert File.exists?("#{@output_folder}#{@expected_file}"), "Expected output file '#{@output_folder}#{@expected_file}'"
+  def test_noparams
+    expected_file = "test/decks/programming.#{@extension}"
+    Lackey_MSE2.init([@deck_file, "-s"])
+    assert File.exists?(expected_file), "Expected output file '#{expected_file}'"
   end
  
-  def test_file_contents
+  def test_output
+    expected_file = "#{@output_folder}/programming.#{@extension}"
+    Lackey_MSE2.init([@deck_file, "-s", "-o", "#{@output_folder}"])
+    assert File.exists?(expected_file), "Expected output file '#{expected_file}'"
+  end
+ 
+  def test_output_file
+    expected_file = "#{@output_folder}/prgd.#{@extension}"
+    Lackey_MSE2.init([@deck_file, "-s", "-o", "#{@output_folder}/prgd"])
+    assert File.exists?(expected_file), "Expected output file '#{expected_file}'"
+  end
+ 
+  def test_name
+    expected_file = "#{@output_folder}/dvorakpgrd.#{@extension}"
+    Lackey_MSE2.init([@deck_file, "-s", "-n", "dvorakpgrd", "-o", "#{@output_folder}"])
+    assert File.exists?(expected_file), "Expected output file '#{expected_file}'"
+  end
+ 
+  def test_output_invalid_name
+    expected_file = "#{@output_folder}/dan_ger_ous.#{@extension}"
+    Lackey_MSE2.init([@deck_file, "-s", "-o", "#{@output_folder}/dan!ger ous::a"])
+    assert File.exists?(expected_file), "Expected output file '#{expected_file}'"
+  end
+ 
+  def test_dryrun
+    expected_file = "#{@output_folder}/noop.#{@extension}"
+    Lackey_MSE2.init([@deck_file, "-s", "-d", "-o", "#{@output_folder}/noop"])
+    assert_equal false, File.exists?(expected_file), "Not expected output file '#{expected_file}'"
+  end
+ 
+  def test_contents
+    expected_file = "#{@output_folder}/prgdz.#{@extension}"
+    Lackey_MSE2.init([@deck_file, "-s", "-o", "#{@output_folder}/prgdz"])
+    
     set_file = nil
-    Zip::File.open("#{@output_folder}#{@expected_file}") do |zip_file|
-      set_file = zip_file.glob(@expected_file_entry).first
+    Zip::File.open(expected_file) do |zip_file|
+      set_file = zip_file.glob('set').first
     end
     
-    assert_not_nil set_file, "Expected file '#{@expected_file_entry}' in '#{@expected_file}'"
+    assert_not_nil set_file, "Expected file 'set' in '#{expected_file}'"
   end
  
 end
